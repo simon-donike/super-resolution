38e9614d23f1f4a126619ceee209afcb3b2f0576
simon-donike <accounts@donike.net>
2021-11-09 13:07:15 +0100

    automatic commit

---
+++ auto_commit.sh
@@ -0,0 +1,9 @@
+cd /home/simon/CDE_UBS/thesis/;
+git pull;
+git status;
+find . -size +10M | sed 's|^\./||g' | cat > .gitignore;
+".DS_Store" >> .gitignore;
+".ipynb_checkpoints" >> .gitignore;
+git add .;
+git commit -m "automatic commit";
+git push -u origin master;
