f1660c86031dda673e0b63709618311b10a4e16a
simon-donike <accounts@donike.net>
2021-12-07 23:25:12 +0100

    added_footprints

--- auto_commit.sh
+++ auto_commit.sh
@@ -1,6 +1,4 @@
 cd /home/simon/CDE_UBS/thesis/;
-git pull;
-git status;
 find . -size +20M | sed 's|^\./||g' | cat > .gitignore;
 echo "*.tif" >> .gitignore;
 echo "*.jp2" >> .gitignore;
@@ -9,6 +7,8 @@ echo ".ipynb_checkpoints" >> .gitignore;
 echo "mac_ignore.sh" >> .gitignore;
 
 echo "auto_commit.sh" >> .gitignore;
+git pull;
+git status;
 git add .;
 git commit -m "automatic commit";
 git push -u origin main;
