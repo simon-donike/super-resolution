8f2805a5672b47a379b6b8d2fd8d0dcb95f113b9
simon-donike <accounts@donike.net>
2021-12-07 23:14:27 +0100

    manual

--- auto_commit.sh
+++ auto_commit.sh
@@ -2,6 +2,8 @@ cd /home/simon/CDE_UBS/thesis/;
 git pull;
 git status;
 find . -size +20M | sed 's|^\./||g' | cat > .gitignore;
+echo "*.tif" >> .gitignore;
+echo "*.jp2" >> .gitignore;
 echo ".DS_Store" >> .gitignore;
 echo ".ipynb_checkpoints" >> .gitignore;
 echo "mac_ignore.sh" >> .gitignore;
