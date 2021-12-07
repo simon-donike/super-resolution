5a4085a2bd7db3e32a9473231df8af2fe095fe05
simon-donike <accounts@donike.net>
2021-12-07 23:21:01 +0100

    automatic commit

--- auto_commit.sh
+++ auto_commit.sh
@@ -7,7 +7,7 @@ echo "*.jp2" >> .gitignore;
 echo ".DS_Store" >> .gitignore;
 echo ".ipynb_checkpoints" >> .gitignore;
 echo "mac_ignore.sh" >> .gitignore;
-echo ".gitignore" >> .gitignore;
+
 echo "auto_commit.sh" >> .gitignore;
 git add .;
 git commit -m "automatic commit";
