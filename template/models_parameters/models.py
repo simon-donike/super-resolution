b9ca210e3cb2050bd32d1b32531cafa4e083c232
simon-donike <accounts@donike.net>
2022-03-02 03:00:08 +0100

    automatic commit

--- template/models_parameters/models.py
+++ template/models_parameters/models.py
@@ -20,3 +20,24 @@ class SRCNN(nn.Module):
         x = self.relu(self.conv2(x))
         x = self.conv3(x)
         return x
+
+
+class SRCNN_extended(nn.Module):
+    # https://keras.io/examples/vision/super_resolution_sub_pixel/
+    # https://mfarahmand.medium.com/cnn-based-single-image-super-resolution-6ffcd39ec993
+    def __init__(self, num_channels=3):
+        super(SRCNN_extended, self).__init__()
+        self.conv1 = nn.Conv2d(num_channels, 64, kernel_size=9, padding=9 // 2)
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=9, padding=9 // 2)
+        self.conv3 = nn.Conv2d(128, 64, kernel_size=9, padding=9 // 2)
+        self.conv4 = nn.Conv2d(64, 32, kernel_size=9, padding=9 // 2)
+        self.conv5 = nn.Conv2d(32, num_channels, kernel_size=5, padding=5 // 2)
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        x = self.relu(self.conv1(x))
+        x = self.relu(self.conv2(x))
+        x = self.relu(self.conv3(x))
+        x = self.relu(self.conv4(x))
+        x = self.conv5(x)
+        return x
